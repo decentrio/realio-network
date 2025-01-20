b09b1bf95257f9c2967dcbb5a8df0e34adf6f027
Hieu Vu <hieuvubk@gmail.com>
2025-01-20 20:33:46 +0700

    build

---
+++ x/asset/genesis.go
@@ -0,0 +1,45 @@
+package asset
+
+import (
+	"context"
+
+	"github.com/realiotech/realio-network/x/asset/keeper"
+	"github.com/realiotech/realio-network/x/asset/types"
+)
+
+// InitGenesis initializes the assets module's state from a provided genesis
+// state.
+func InitGenesis(ctx context.Context, k keeper.Keeper, genState types.GenesisState) {
+	err := k.Params.Set(ctx, genState.Params)
+	if err != nil {
+		panic(err)
+	}
+	for _, token := range genState.Tokens {
+		err := k.Token.Set(ctx, token.Symbol, token)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx context.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+	params, err := k.Params.Get(ctx)
+	if err != nil {
+		panic(err)
+	}
+	genesis.Params = params
+	tokens := []types.Token{}
+	err = k.Token.Walk(ctx, nil, func(_ string, token types.Token) (stop bool, err error) {
+		tokens = append(tokens, token)
+		return false, nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	genesis.Tokens = tokens
+	// this line is used by starport scaffolding # genesis/module/export
+
+	return genesis
+}
---
+++ x/asset/module.go
@@ -0,0 +1,187 @@
+package asset
+
+import (
+	"context"
+	"encoding/json"
+
+	// this line is used by starport scaffolding # 1
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+
+	"cosmossdk.io/core/appmodule"
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
+	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+
+	"github.com/realiotech/realio-network/x/asset/exported"
+	"github.com/realiotech/realio-network/x/asset/keeper"
+	"github.com/realiotech/realio-network/x/asset/types"
+)
+
+var (
+	_ module.AppModuleBasic      = AppModule{}
+	_ module.AppModuleSimulation = AppModule{}
+	// _ module.HasGenesis          = AppModule{}
+	// _ module.HasServices         = AppModule{}
+
+	_ appmodule.AppModule = AppModule{}
+)
+
+// ConsensusVersion defines the current x/asset module consensus version.
+const ConsensusVersion = 2
+
+// ----------------------------------------------------------------------------
+// AppModuleBasic
+// ----------------------------------------------------------------------------
+
+// AppModuleBasic implements the AppModuleBasic interface for the capability module.
+type AppModuleBasic struct {
+	cdc codec.BinaryCodec
+}
+
+var _ module.AppModuleBasic = AppModuleBasic{}
+
+func NewAppModuleBasic(cdc codec.BinaryCodec) AppModuleBasic {
+	return AppModuleBasic{cdc: cdc}
+}
+
+// Name returns the capability module's name.
+func (AppModuleBasic) Name() string {
+	return types.ModuleName
+}
+
+func (AppModuleBasic) RegisterCodec(cdc *codec.LegacyAmino) {
+	types.RegisterCodec(cdc)
+}
+
+func (AppModuleBasic) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	types.RegisterCodec(cdc)
+}
+
+// RegisterInterfaces registers the module's interface types
+func (a AppModuleBasic) RegisterInterfaces(reg cdctypes.InterfaceRegistry) {
+	types.RegisterInterfaces(reg)
+}
+
+// DefaultGenesis returns the capability module's default genesis state.
+// func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
+// 	return cdc.MustMarshalJSON(types.DefaultGenesis())
+// }
+
+// ValidateGenesis performs genesis state validation for the capability module.
+// func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, _ client.TxEncodingConfig, bz json.RawMessage) error {
+// 	var genState types.GenesisState
+// 	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
+// 		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
+// 	}
+// 	return genState.Validate()
+// }
+
+// RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
+func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
+	if err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
+}
+
+// GetTxCmd returns the capability module's root tx command.
+// func (a AppModuleBasic) GetTxCmd() *cobra.Command {
+// 	return cli.GetTxCmd()
+// }
+
+// GetQueryCmd returns the capability module's root query command.
+// func (AppModuleBasic) GetQueryCmd() *cobra.Command {
+// 	return cli.GetQueryCmd()
+// }
+
+// ----------------------------------------------------------------------------
+// AppModule
+// ----------------------------------------------------------------------------
+
+// AppModule implements the AppModule interface for the capability module.
+type AppModule struct {
+	AppModuleBasic
+
+	// legacySubspace is used solely for migration of x/params managed parameters
+	legacySubspace exported.Subspace
+
+	keeper     keeper.Keeper
+	bankKeeper types.BankKeeper
+}
+
+func NewAppModule(
+	cdc codec.Codec,
+	keeper keeper.Keeper,
+	bankKeeper types.BankKeeper,
+	ss exported.Subspace,
+) AppModule {
+	return AppModule{
+		AppModuleBasic: NewAppModuleBasic(cdc),
+		keeper:         keeper,
+		bankKeeper:     bankKeeper,
+		legacySubspace: ss,
+	}
+}
+
+// IsOnePerModuleType implements the depinject.OnePerModuleType interface.
+func (AppModule) IsOnePerModuleType() {}
+
+// IsAppModule implements the appmodule.AppModule interface.
+func (AppModule) IsAppModule() {}
+
+// Name returns the capability module's name.
+func (am AppModule) Name() string {
+	return am.AppModuleBasic.Name()
+}
+
+// RegisterServices registers a GRPC query service to respond to the
+// module-specific GRPC queries.
+// func (am AppModule) RegisterServices(cfg module.Configurator) {
+// 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
+// 	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServerImpl(am.keeper))
+
+// 	m := keeper.NewMigrator(am.keeper, am.legacySubspace)
+
+// 	if err := cfg.RegisterMigration(types.ModuleName, 1, m.Migrate1to2); err != nil {
+// 		panic(fmt.Sprintf("failed to migrate x/%s from version 1 to 2: %v", types.ModuleName, err))
+// 	}
+// }
+
+// RegisterInvariants registers the capability module's invariants.
+func (am AppModule) RegisterInvariants(_ sdk.InvariantRegistry) {}
+
+// InitGenesis performs the capability module's genesis initialization It returns
+// no validator updates.
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, gs json.RawMessage) {
+	var genState types.GenesisState
+	// Initialize global index to index in genesis state
+	cdc.MustUnmarshalJSON(gs, &genState)
+
+	InitGenesis(ctx, am.keeper, genState)
+}
+
+// ExportGenesis returns the capability module's exported genesis state as raw JSON bytes.
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	genState := ExportGenesis(ctx, am.keeper)
+	return cdc.MustMarshalJSON(genState)
+}
+
+// ConsensusVersion implements ConsensusVersion.
+func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
+
+func (am AppModule) GenerateGenesisState(_ *module.SimulationState) {
+}
+
+func (AppModule) ProposalMsgs(_ module.SimulationState) []simtypes.WeightedProposalMsg {
+	return []simtypes.WeightedProposalMsg{}
+}
+
+func (am AppModule) RegisterStoreDecoder(_ simtypes.StoreDecoderRegistry) {
+}
+
+func (am AppModule) WeightedOperations(_ module.SimulationState) []simtypes.WeightedOperation {
+	return []simtypes.WeightedOperation{}
+}
