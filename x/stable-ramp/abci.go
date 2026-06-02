f107d810815ff47405a67d6b6469d56b1dbbb8aa
Hieu Vu <hieuvubk@gmail.com>
2026-06-02 12:24:38 +0700

    fullfill deposit flow

--- x/stable-ramp/abci.go
+++ x/stable-ramp/abci.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"cosmossdk.io/collections"
+	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
@@ -36,11 +37,33 @@ func BeginBlocker(ctx context.Context, k keeper.Keeper) error {
 
 		switch pkg.Action {
 		case types.PackageAction_PACKAGE_ACTION_DEPOSIT:
-			// TODO: add deposit package fulfillment logic here.
+			receiver, err := sdk.AccAddressFromBech32(pkg.ReceiverAddr)
+			if err != nil {
+				return true, err
+			}
+			depositCoin := sdk.NewCoin(pkg.Denom, math.NewIntFromUint64(pkg.Amount))
+			if err := k.FulfillDeposit(ctx, receiver, sdk.Coins{depositCoin}); err != nil {
+				return true, err
+			}
 			if err := k.PendingPackages.Remove(ctx, key); err != nil {
 				return true, err
 			}
+			sdkCtx.EventManager().EmitEvent(
+				sdk.NewEvent(
+					types.EventTypeDepositClaimConfirm,
+					sdk.NewAttribute(types.AttributeKeyReceiver, pkg.ReceiverAddr),
+					sdk.NewAttribute(types.AttributeKeyAmount, strconv.FormatUint(pkg.Amount, 10)),
+					sdk.NewAttribute(types.AttributeKeyDenom, pkg.Denom),
+					sdk.NewAttribute(types.AttributeKeyConnection, pkg.ConnectionId),
+					sdk.NewAttribute(types.AttributeKeyNonce, strconv.FormatUint(pkg.Nonce, 10)),
+					sdk.NewAttribute(types.AttributeKeyApprovals, strconv.FormatUint(uint64(trackedPackage.Approvals), 10)),
+				),
+			)
 		case types.PackageAction_PACKAGE_ACTION_WITHDRAW:
+			burnCoin := sdk.NewCoin(pkg.Denom, math.NewIntFromUint64(pkg.Amount))
+			if err := k.BurnEscrowedCoins(ctx, sdk.Coins{burnCoin}); err != nil {
+				return true, err
+			}
 			trackedPackageBz, err := types.ModuleCdc.Marshal(&trackedPackage)
 			if err != nil {
 				return true, err
