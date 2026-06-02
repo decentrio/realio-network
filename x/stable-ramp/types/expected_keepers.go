1dd21a3cd6647bc69fd1713964c4d04538358997
Hieu Vu <hieuvubk@gmail.com>
2026-06-02 12:23:42 +0700

    helper

--- x/stable-ramp/types/expected_keepers.go
+++ x/stable-ramp/types/expected_keepers.go
@@ -9,4 +9,8 @@ import (
 // BankKeeper defines the banking methods required by the stable-ramp module.
 type BankKeeper interface {
 	GetBalance(ctx context.Context, addr sdk.AccAddress, denom string) sdk.Coin
+	MintCoins(ctx context.Context, moduleName string, amounts sdk.Coins) error
+	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+	SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+	BurnCoins(ctx context.Context, name string, amt sdk.Coins) error
 }
